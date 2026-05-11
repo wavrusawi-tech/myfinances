ce021ad4c60c4d409b6846ab9437a3b5a7280a22
William <wavrusawi@gmail.com>
2026-05-11 18:15:37 -0500

    Reordered code

--- main.py
+++ main.py
@@ -3,18 +3,12 @@ import hashlib
 from sqlalchemy import Column, String, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-engine = create_engine("sqlite:///mydb.sqlite3", echo=True)
-
-class Base(DeclarativeBase):
-    pass
-
-SessionLocal = sessionmaker(bind=engine)
 def hash_text(text: str) -> str:
     return hashlib.sha256(text.encode()).hexdigest()
 
-Base.metadata.create_all(engine)
 
-print(text2art("MyFinances", font='block'))
+class Base(DeclarativeBase):
+    pass
 
 class User(Base):
     __tablename__ = 'user'
@@ -22,3 +16,13 @@ class User(Base):
     def __init__(self):
         self.username = Column(String, primary_key=True)
         self.password = Column(String)
+
+engine = create_engine("sqlite:///mydb.sqlite3", echo=True)
+
+
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base.metadata.create_all(engine)
+
+print(text2art("MyFinances", font='block'))
